3a53ca05276b01d86a0c78dac0b50b84aa7a9c4f
Pana <pana.wang@outlook.com>
2024-06-14 15:20:24 +0800

    add contract call

--- tests/call.test.js
+++ tests/call.test.js
@@ -85,3 +85,64 @@ test('two kind gasPrice', async () => {
       checkError(e, {code: -32000, message: "both gasPrice and (maxFeePerGas or maxPriorityFeePerGas) specified"})
     }
 });
+
+
+// 0x43E04C3D393C9F7C3bEA58d171Bc69260f5519Df
+
+test('require failed call', async () => {
+  let txMeta = {
+    to: '0x43E04C3D393C9F7C3bEA58d171Bc69260f5519Df',
+    data: '0xcd16ecbf0000000000000000000000000000000000000000000000000000000000000005'
+  };
+
+  try {
+      let res = await provider.call(txMeta);
+    throw new Error('should not reach here');
+  } catch (e) {
+    let expected = {
+      code: 3, 
+      message: "execution reverted: input must be greater than 10", 
+      data: '0x08c379a00000000000000000000000000000000000000000000000000000000000000020000000000000000000000000000000000000000000000000000000000000001d696e707574206d7573742062652067726561746572207468616e203130000000'
+    };
+    checkError(e, expected)
+  }
+});
+
+test('revert call', async () => {
+  let txMeta = {
+    to: '0x43E04C3D393C9F7C3bEA58d171Bc69260f5519Df',
+    data: '0x209877670000000000000000000000000000000000000000000000000000000000000005'
+  };
+
+  try {
+      let res = await provider.call(txMeta);
+    throw new Error('should not reach here');
+  } catch (e) {
+    let expected = {
+      code: 3, 
+      message: "execution reverted: Input must be greater than 10", 
+      data: '0x08c379a00000000000000000000000000000000000000000000000000000000000000020000000000000000000000000000000000000000000000000000000000000001d496e707574206d7573742062652067726561746572207468616e203130000000'
+    };
+    checkError(e, expected)
+  }
+});
+
+// error InsufficientBalance(uint balance, uint withdrawAmount);
+test('custom error call', async () => {
+  let txMeta = {
+    to: '0x43E04C3D393C9F7C3bEA58d171Bc69260f5519Df',
+    data: '0x75f7286c0000000000000000000000000000000000000000000000000000000000000005'
+  };
+
+  try {
+      let res = await provider.call(txMeta);
+    throw new Error('should not reach here');
+  } catch (e) {
+    let expect = {
+      code: 3, 
+      message: "execution reverted", 
+      data: '0xcf47918100000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000005'
+    }
+    checkError(e, expect);
+  }
+});
